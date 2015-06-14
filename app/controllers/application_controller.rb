80f868afef3280086753db8e7328271d61699665
mufasa72 <aetherisreal42@gmail.com>
2015-06-13 22:53:42 -0700

    Add flights link [1/1]
    [Problem]
    There is no link to the flights page
    
    [Solution]
    Add a link to the flights page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,26 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   attr_accessor :selected_map
 
+  helper_method :get_nav_link
+
   def initialize
     super
     @selected_map = Hash.new(false)
+  end
+
+  def get_nav_link(controller, action, id)
+    is_selected = @selected_map[id.to_sym]
+
+    if is_selected
+      options = '#'
+      navbar_class = 'navbar-selected'
+    else
+      options = {:controller => controller, :action => action}
+      navbar_class = 'navbar-unselected'
+    end
+
+    view_context.link_to options, :class => 'navbar-link' do
+      view_context.content_tag(:div, '', :id => id, :class => navbar_class)
+    end
   end
 end
