6bf14ab78d846ffe1d8b0eb2e5bfef80ef7bee65
mufasa72 <aetherisreal42@gmail.com>
2015-06-15 23:41:24 -0700

    Add photos from hike [1/1]
    [Problem]
    There are no photos to be seen on the "photos" page
    
    [Solution]
    Add photos from the hike to Rattlesnake Ridge. Currently, this directory is hardcoded instead of looking at each folder
    in the "./images/photos" directory.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,5 +5,26 @@ class HomeController < ApplicationController
 
   def photos
     @selected_map[:photos] = true
+
+    # read metadata file and get the original files
+    metadata_file_name          = "#{@@PHOTO_DIR}/metadata"
+    if File.exists? (metadata_file_name)
+      File.open(metadata_file_name) do |metadata_file|
+        metadata      = Crack::JSON.parse(metadata_file.read)
+        @title = metadata['title']
+
+        thumbnail_dir = metadata['thumbnail_dir']
+        @image_paths  = Dir.glob("#{@@PHOTO_DIR}/#{thumbnail_dir}/*").map { |path|
+          path.slice(/photos\/.*/)
+        }
+      end
+    else
+      @image_paths = []
+    end
   end
+
+  private
+
+  @@IMAGE_DIR = 'app/assets/images/'
+  @@PHOTO_DIR = "#{@@IMAGE_DIR}/photos/hike_10-5-2015/"
 end
