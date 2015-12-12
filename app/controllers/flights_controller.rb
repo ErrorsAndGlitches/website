cbd51fd669de4dbb35211989072da4caec37f2a6
mufasa72 <aetherisreal42@gmail.com>
2015-12-12 01:58:53 -0800

    Fix flight index page with new models etc

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -2,19 +2,19 @@ class FlightsController < ApplicationController
   skip_before_filter :verify_authenticity_token, :only => :flight_chart
 
   def index
-    @selected_tab   = :flights
-    @flight_queries = FlightQuery.all
+    @selected_tab = :flights
+    @trips        = Trip.all
   end
 
   def flight
     @selected_tab = :flights
 
-    id      = params[:id]
-    @flight = FlightQuery.find(id)
+    id    = params[:id]
+    @trip = Trip.find(id)
   end
 
   def flight_chart
-    id            = params[:id]
-    @flight_query = FlightQuery.find(id)
+    id    = params[:id]
+    @trip = Trip.find(id)
   end
 end
